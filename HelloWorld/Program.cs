a63daed5dd5d3b3c83f0474608f580b58f3370c0
Flannan Brody <fpabrody@gmail.com>
2019-12-14 20:48:11 -0800

    Adding a change from user 1

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,7 +7,7 @@ namespace HelloWorld
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
-            Console.WriteLine("Merge change from user2");
+            Console.WriteLine("Merge change from user1...new change");
             var runner = new Runner();
             runner.Run();
         }
