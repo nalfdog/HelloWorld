3814df2fde685e478b41b35504c5fc022159fa21
Flannan Brody <fpabrody@gmail.com>
2019-12-14 19:33:42 -0800

    Adding another second change to calculator branch

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,7 +7,7 @@ namespace HelloWorld
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
-            Console.WriteLine("Goodbye Flannan");
+            Console.WriteLine("Goodbye hello Flannan");
             var runner = new Runner();
             runner.Run();
         }
