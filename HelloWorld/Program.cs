03dfc16b0b1d4f06bd64dfc8ff07d1f222e6f638
Flannan Brody <fpabrody@gmail.com>
2019-12-14 20:04:55 -0800

    Merging for user2

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,7 +7,7 @@ namespace HelloWorld
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
-            Console.WriteLine("Goodbye hello Flannan");
+            Console.WriteLine("Merge change from user2");
             var runner = new Runner();
             runner.Run();
         }
