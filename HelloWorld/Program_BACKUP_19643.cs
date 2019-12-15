6881cf41fc080af078e265eb66d963872d8827d5
Flannan Brody <fpabrody@gmail.com>
2019-12-14 20:34:01 -0800

    Big big commit....

---
+++ HelloWorld/Program_BACKUP_19643.cs
@@ -0,0 +1,19 @@
+﻿﻿using System;
+
+namespace HelloWorld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+<<<<<<< .merge_file_UMcwz4
+            Console.WriteLine("Merge change from user1");
+=======
+            Console.WriteLine("Merge change from user2");
+>>>>>>> .merge_file_w5h5Gz
+            var runner = new Runner();
+            runner.Run();
+        }
+    }
+}
---
+++ HelloWorld/Program_REMOTE_19643.cs
@@ -0,0 +1,15 @@
+﻿﻿using System;
+
+namespace HelloWorld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Merge change from user2");
+            var runner = new Runner();
+            runner.Run();
+        }
+    }
+}
